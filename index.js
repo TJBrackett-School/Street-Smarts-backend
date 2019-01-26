18b67b932c3f4c44d2f2d7633abd23c254d32d25
TJBrackett-School <thomas750566@my.yosemite.edu>
2019-01-25 23:43:44 -0800

    Added api backend using express.

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+var corOptions = {
+	origin: 'http://www.example.com',
+	optionsSuccessStatus: 200;
+};
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.listen(8000m (*) => {
+	console.log('Server started.');
+});
+
+app.route('/api/books').get((req, res) => {
+	res.send({
+		books: [{title: '1984'}, {title: 'Game of Thrones'}]
+	});
+});
+
+app.route('/api/books/:title').get((req, res) => {
+	const requestedTitle = req.params['title'];
+	res.send({title:requestedTitle});
+});
+
+app.route('/api/books').post((req, res) => {
+	res.send(201, req.body);
+});
+
+app.route('/api/books/:title').put((req, res) => {
+	res.send(200, req.body);
+});
+
+app.route('/api/books/:title').delete((req, res) => {
+	res.sendStatus(204);
+});
