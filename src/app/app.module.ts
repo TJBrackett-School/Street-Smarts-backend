23b4efc790531e3687b41084972ac84e59903bcd
Michael Edwards <leahcim12000@gmail.com>
2019-01-25 21:50:51 -0800

    Adding Search and other Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
