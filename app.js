0d2452cd93ce41bd04c642858efee031670f3591
TJBrackett-School <thomas750566@my.yosemite.edu>
2019-03-15 19:25:29 -0700

    Added post route for register and variable routes for login and home.

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 
-//Current directory + /views/ folder
+//Current directory + /public/html/ folder
 var viewsPath = __dirname + '/public/html/';
 var apiPath = __dirname + '/api/';
 
 
 app.use('/', router);
-//Supports json encoded bodies
+//Body-parser handles incoming POST requests
 app.use(bodyParser.json());
-//Supports encoded bodies
 app.use(bodyParser.urlencoded({extended: true}));
 
 //Simple get routing for serving the views to the user
@@ -24,8 +23,17 @@ router.get('/login', (req, res) => {
     res.sendFile(viewsPath + 'login.html');
 });
 
+router.get('/login/:user', (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+});
+
 router.get('/home', (req, res) => {
     res.sendFile(viewsPath + 'home.html');
+});
+
+router.get('/home/:search', (req, res) => {
+    res.send(req.params);
 });
 
 router.get('/account', (req, res) => {
@@ -34,6 +42,12 @@ router.get('/account', (req, res) => {
 
 router.get('/register', (req, res) => {
     res.sendFile(viewsPath + 'register.html');
+});
+router.post('/register/:user', (req, res) => {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    var password = req.body.password;
 });
 //Throw 404 error if route not found
 app.use('*', (req, res) => {
